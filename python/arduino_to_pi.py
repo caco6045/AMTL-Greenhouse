82892dff7df35bc8b8a020d1cfe6ea98a5fa756e
ryanwood3 <ryan.wood-1@colorado.edu>
2018-04-08 17:45:11 -0600

    dataPull function

--- python/arduino_to_pi.py
+++ python/arduino_to_pi.py
@@ -4,32 +4,31 @@
 
 import serial
 
-def main():
+def dataPull():
 
 	ser = serial.Serial('COM4',9600)
 	print("Connected to: " + ser.portstr)
 
-	while True:
-		floatdata=ser.readline()
-		tempdata=ser.readline()
-		humdata=ser.readline()
-		phdata=ser.readline()
-		
-		floatdata_i=int(floatdata)
-		floatdata_b=bool(floatdata_i)
-		tempdata_f=float(tempdata)
-		humdata_f=float(humdata)
-		phdata_f=float(phdata)
-		
-		if(floatdata_b==0):
-			ser.write("A")
-		else:
-			ser.write("B")
-		
-		print(floatdata_b)
-		print(tempdata_f)
-		print(humdata_f)
-		print(phdata_f)
-		print
+	floatdata=ser.readline()
+	tempdata=ser.readline()
+	humdata=ser.readline()
+	phdata=ser.readline()
+	
+	floatdata_i=int(floatdata)
+	floatdata_b=bool(floatdata_i)
+	tempdata_f=float(tempdata)
+	humdata_f=float(humdata)
+	phdata_f=float(phdata)
+	
+	if(floatdata_b==0):
+		ser.write("A")
+	else:
+		ser.write("B")
+	
+	print(floatdata_b)
+	print(tempdata_f)
+	print(humdata_f)
+	print(phdata_f)
+	print
 
-main()
+dataPull()
